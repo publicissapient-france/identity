91ccbdadd2fce932aff67444ea41fabee5ff1ebb
Jean-Louis Rigau <jeanlouis.rigau@gmail.com>
2016-06-01 02:31:55 +0200

    Add Hit button and publish to Redis

--- identity.go
+++ identity.go
@@ -1,27 +1,62 @@
 package main
 
 import (
+	"github.com/caarlos0/env"
+	"github.com/garyburd/redigo/redis"
 	"html/template"
 	"net/http"
 	"os"
 	"log"
 	"io"
 	"encoding/json"
+	"strconv"
 )
 
-type Identity struct {
-	Name     string        `json:"name"`
-	Filename string        `json:"filename"`
-	Hostname string        `json:"hostname"`
-	Hits     int           `json:"hits"`
+type Config struct {
+	RedisHost    string `env:"REDIS_HOST" envDefault:"redis"`
+	RedisPort    int    `env:"REDIS_PORT" envDefault:"6379"`
+	RedisChannel string `env:"REDIS_CHANNEL" envDefault:"service.hit"`
 }
 
-var templates = template.Must(template.ParseFiles("identity.html"))
+var cfg = Config{}
+
+func dial() (redis.Conn, error) {
+	c, err := redis.Dial("tcp", cfg.RedisHost + ":" + strconv.Itoa(cfg.RedisPort))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func publish(channel, value interface{}) error {
+	c, err := dial()
+
+	if err != nil {
+		return err
+	}
+
+	defer c.Close()
+
+	c.Do("PUBLISH", channel, value)
+
+	return nil
+}
+
+type Identity struct {
+	Name     string `json:"name"`
+	Filename string `json:"filename"`
+	Hostname string `json:"hostname"`
+	Hits     int    `json:"hits"`
+}
+
+var identity, err = loadIdentity()
 
 func getImage(url string, filename string) {
-	response, e := http.Get(url)
-	if e != nil {
-		log.Fatal(e)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	defer response.Body.Close()
@@ -63,7 +98,7 @@ func loadIdentity() (*Identity, error) {
 	return &Identity{Name: name, Filename: filename, Hostname: hostname, Hits: 0}, nil
 }
 
-var identity, err = loadIdentity()
+var templates = template.Must(template.ParseFiles("identity.html"))
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, *Identity)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -87,10 +122,28 @@ func jsonHandler(w http.ResponseWriter, r *http.Request, identity *Identity) {
 	w.Write(js)
 }
 
-func identityHandler(w http.ResponseWriter, r *http.Request, identity *Identity) {
+func hitHandler(w http.ResponseWriter, r *http.Request, identity *Identity) {
 	identity.Hits = identity.Hits + 1
 
-	err := templates.ExecuteTemplate(w, "identity.html", identity)
+	js, err := json.Marshal(identity)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = publish(cfg.RedisChannel, js)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/identity", http.StatusFound)
+}
+
+func identityHandler(w http.ResponseWriter, r *http.Request, identity *Identity) {
+	err = templates.ExecuteTemplate(w, "identity.html", identity)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -102,7 +155,10 @@ func staticHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	env.Parse(&cfg)
+
 	http.HandleFunc("/identity", makeHandler(identityHandler))
+	http.HandleFunc("/identity/hit", makeHandler(hitHandler))
 	http.HandleFunc("/identity/json", makeHandler(jsonHandler))
 
 	http.HandleFunc("/static/", staticHandler)
